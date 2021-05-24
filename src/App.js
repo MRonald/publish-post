6f8143602cbf7296d3b28a3a25c3a681bb648273
MRonald <ronaldmichael918@gmail.com>
2021-05-24 16:02:40 -0300

    Ajustes iniciais | Adição do editor config | Código base do desafio

--- src/App.js
+++ src/App.js
@@ -1,7 +1,60 @@
+import { useState } from "react";
+
 export default function App() {
-  return (
-    <div>
-      Meu app
-    </div>
-  );
+
+    const [text, setText] = useState("");
+    const [postList, setPostList] = useState([
+        { "text": "lorem ipsum dolor sit amet", "date": "moment().format('DD/MM/YYYY HH:mm')" }
+    ]);
+
+    function onChangeText(event) {
+        setText(event.target.value);
+    }
+
+    function publishPost(event) {
+        event.preventDefault();
+        const post = {
+            "text": text,
+            "date": "moment().format('DD/MM/YYYY HH:mm')"
+        }
+        console.log(postList);
+        setPostList(postList.push(post));
+        setText("");
+    }
+
+    function deletePost(event) {
+        event.preventDefault();
+    }
+
+    return (
+        <div className="main-wrapper">
+            <div className="box">
+                <form className="form">
+                    <div class="field">
+                        <label for="name-1">O que está acontecendo?</label>
+                        <div class="control">
+                            <textarea value={text} name="name-1" rows="5" onChange={onChangeText} class="input" />
+                        </div>
+                    </div>
+                    <div class="field">
+                        <div class="control">
+                            <button onClick={publishPost} class="button is-dark">Publicar</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+
+            <div className="post-list">
+                {
+                    postList.map((post, index) => (
+                        <div key={index} className="post-item">
+                            <span className="date">{post.date}</span>
+                            <p>{post.text}</p>
+                            <button onClick={deletePost} className="button">apagar</button>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
+    );
 }
